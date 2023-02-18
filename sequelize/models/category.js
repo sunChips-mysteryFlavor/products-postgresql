086941866aa14739f9679dc90e5aae0ed4be5ced
samuel ho <samuelho343@gmail.com>
2023-02-17 22:06:18 -0800

    migration featurekeys

--- sequelize/models/category.js
+++ sequelize/models/category.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.product, { foreignKey: 'category_id' });
+      this.hasMany(models.products, { foreignKey: 'category_id' });
     }
   }
   category.init(
