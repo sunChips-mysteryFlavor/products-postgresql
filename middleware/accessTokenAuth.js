f1b506de46dd1b6738b41e55d68e59e45e82d90d
samuel ho <samuelho343@gmail.com>
2023-02-19 19:50:07 -0800

    remove log

--- middleware/accessTokenAuth.js
+++ middleware/accessTokenAuth.js
@@ -7,7 +7,6 @@ const key = 'rrMbJsGJ4Pxxs0SVn8lfFzzeIhp8LQEJ';
 module.exports = (req, res, next) => {
   const [ivBase64, encryptedData] = req.headers.authorization.split('|');
   const iv = Buffer.from(ivBase64, 'base64');
-  console.log(iv, encryptedData, 'here');
 
   let decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
